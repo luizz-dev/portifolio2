53be0a1b39a085b82434227d79d6964317685597
Luiz Eduardo <luizleitefe@gmail.com>
2025-09-17 16:09:35 -0300

    mobile do contato finalizado

--- modules/redirecinar.js
+++ modules/redirecinar.js
@@ -1,22 +1,36 @@
 export function redirecionarCliente() {
-  const contatoWpp = document.getElementById("whatsapp-contato");
-  if (contatoWpp) {
-    contatoWpp.addEventListener("click", () => {
-      window.location.href = "https://wa.me/+5511958212008";
-    });
-  }
+  const contatosWpp = [
+    document.getElementById("whatsapp-contato"),
+    document.getElementById("whatsapp-contato-m"),
+  ];
 
-  const contatoGitHub = document.getElementById("github");
-  if (contatoGitHub) {
-    contatoGitHub.addEventListener("click", () => {
+  contatosWpp.forEach((el) => {
+    if (el) {
+      el.addEventListener("click", () => {
+        window.location.href = "https://wa.me/+5511958212008";
+      });
+    }
+  });
+
+  const contatosGitHub = [
+    document.getElementById("github"),
+    document.getElementById("github-m"),
+  ];
+
+  contatosGitHub.forEach((el) => {
+    el.addEventListener("click", () => {
       window.location.href = "https://github.com/luizz-dev";
     });
-  }
+  });
 
-  const contatoLinkedin = document.getElementById("linkedin-contato");
-  if (contatoLinkedin) {
-    contatoLinkedin.addEventListener("click", () => {
+  const contatosLinkedin = [
+    document.getElementById("linkedin-contato"),
+    document.getElementById("linkedin-contato-m"),
+  ];
+
+  contatosLinkedin.forEach((el) => {
+    el.addEventListener("click", () => {
       window.location.href = "https://www.linkedin.com/in/luiz-alves-leite/";
     });
-  }
+  });
 }
